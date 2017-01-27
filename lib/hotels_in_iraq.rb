0b0284627a74217425aacd300c89d7a6dd461bc9
Sewar Abdullah <abdullahsewar@gmail.com>
2017-01-27 11:19:43 +0000

    scraping

--- lib/hotels_in_iraq.rb
+++ lib/hotels_in_iraq.rb
@@ -1,2 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./hotels_in_iraq/version"
-require_relative './hotels_in_iraq/cli'
+require_relative "./hotels_in_iraq/cli"
+require_relative  "./hotels_in_iraq/hotels"
--- lib/hotels_in_iraq/cli.rb
+++ lib/hotels_in_iraq/cli.rb
@@ -1,6 +1,51 @@
 class HotelsInIraq::CLI
 
   def call
-    puts "                    << WELCOME TO THE BEST HOTELS IN IRAQ >>"
+    puts "
+          ________________________________________________________________
+          |                                                      ____    |
+          |         <<   WELCOMETOTHEBESTHOTELSINIRAQ    >>     |    |   |
+          |              _____________________________          |    |   |
+          |                   ____________                   ___|    |__ |
+          |                   |          |                   |          ||
+          |                   |      ____|______          ___|___       ||
+          |     ______________|__    |         |_________|      |       ||
+          |     |               |    |         |         |      |       ||
+          |     |               |    |         |         |      |       ||
+          |     |               |    |         |         |      |       ||
+          |     |               |____|         |         |      |       ||
+          |_____|______________________________|_________|______|_______||"
+         puts ".                                                                 ."
+     best_hotels
+     menu
+     finish
+  end
+  def best_hotels
+    @hotels = HotelsInIraq::Hotel.today
+    @hotels.each_with_index do |hotel, i|
+      puts "#{i+1}. #{hotel.name} - #{hotel.location} - #{hotel.rate}"
+    end
+  end
+
+  def menu
+    puts "Please, Inter the hotel's number you like to see more information or type list to see our Hotel's list or type exit to exit :"
+    input = nil
+    while input != "exit"
+      input= gets.strip.downcase
+      if input.to_i > 0
+        the_hotel= @hotels[input.to_i-1]
+        puts "#{the_hotel.name} - #{the_hotel.location} - #{the_hotel.rate}"
+        puts "Please, Inter the hotel's number you like to see more information or type list to see our Hotel's list or type exit to exit :"
+
+      elsif input == "list"
+        best_hotels
+      else
+        puts "not provided ....type a valid number"
+      end
+    end
+  end
+
+  def finish
+    puts "thanks for "
   end
 end
